e71daab05840bea225e7cf437ae10b819e87b754
Lorena <lorena.giddens@outlook.com>
2018-06-18 17:42:07 +0000

    Done.

---
+++ lib/class_methods_module.rb
@@ -0,0 +1,8 @@
+# Extending Module Methods As Class Methods
+# use the extend keyword
+
+module MetaDancing
+  def metadata
+    "This class produces objects that love to dance."
+  end
+end
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,20 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
+
+# Step 2: Define the Classes aka Tell the Kid Class to include the capabilities of the Dance module
+# Step 3: Include the Module aka Lend the Two Classes All of the Methods of the Dance Module with Include keyword
+# used as instance methods
+
+# extend the MetaDancing module
+
+class Kid
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
